4e999ec32bc7085e9fe78656bfd3ee88798e64ae
Tharinda Rodrigo <tharindarodrigo@gmail.com>
2020-02-12 08:38:55 +0530

    Added Teams

---
+++ database/migrations/2020_02_12_025947_create_teams_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTeamsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teams', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name')->index();
+            $table->unsignedBigInteger('enterprise_id')->index();
+            $table->timestamps();
+
+            $table->foreign('enterprise_id')->references('id')->on('enterprises');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teams');
+    }
+}
